74529f102aa1ceadb20487ec8ba3c1beafb1cf3c
Irish Rivera <162866325+kelleeerrrr@users.noreply.github.com>
2024-05-16 23:57:42 +0800

    Create score.py

---
+++ utils/score.py
@@ -0,0 +1,6 @@
+class Score:
+    def __init__(self, username, password, points, wins):
+        self.username = username
+        self.password = password
+        self.points = 0
+        self.wins = 0
